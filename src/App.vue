b0c0cf631de18237f209733ec45f1c302a39a879
Eikcaj.1998 <jw220998@gmail.com>
2022-08-23 11:49:42 +0200

    install bootsrtap

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
-
+  <TheHeader/>
+  <TheMain/>
+  <TheFooter/>
   </div>
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+import TheMain from "./components/TheMain.vue";
+import TheFooter from "./components/TheFooter.vue";
 
 export default {
   name: 'App',
   components: {
-  }
+    TheHeader,
+    TheMain,
+    TheFooter,
+}
 }
 </script>
 
 <style lang="scss">
-
+@import "~bootstrap/scss/bootstrap.scss";
 </style>
