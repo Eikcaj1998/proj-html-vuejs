c5597fa376195599e65a9d030dd486c6a26a2fe1
Eikcaj.1998 <jw220998@gmail.com>
2022-08-23 14:14:59 +0200

    nav-bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="d-flex justify-content-between align-items-center">
+        <a href="#">
+        <img src="../assets/img/logo-sidearea-1.png" alt="logo"></a>
+        </div>
+            <ul class="d-flex">
+            <li v-for="link in links" :key="link" class="px-4 py-1">
+                    <a class="nav-link text-black" href="#">{{link}}</a>
+            </li>
+            <li>
+            <img src="../assets/svg/svg-1.svg" alt="">
+            </li>
+            </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'NavBar',
+    data() {
+        return {
+            links:[
+                'HOME', 
+                'PAGES',
+                'PORTFOLIO',
+                'BLOG',
+                'SHOP',
+                'ELEMENTS'
+                ]
+        }
+    },
+    }
+</script>
+
+<style lang="scss" scoped>
+ul{
+    list-style-type: none;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <nav class="navbar py2">
+        <div class="container-fluid">
+            <NavBar/>
+            <JumboTron/>
+        </div>
+    </nav>
+</template>
+<script>
+import NavBar from "./NavBar.vue";
+import JumboTron from "./JumboTron.vue";
+
+export default {
+    name:"TheHeader",
+    components: {
+    NavBar,
+    JumboTron
+}
+}
+</script>
