10d7c080517cc5e26ad0a2c21f059b02a67cbd1a
Eikcaj.1998 <jw220998@gmail.com>
2022-08-24 10:34:06 +0200

    create main components

---
+++ src/components/TheMain.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <GridImg/>
+        <WorkingProcess/>
+        <ContactMessage/>
+        <EmilyFields/>
+    </div>
+</template>
+<script>
+import GridImg from "./GridImg.vue";
+import WorkingProcess from "./WorkingProcess.vue";
+import ContactMessage from "./ContactMessage.vue";
+import EmilyFields from "./EmilyFields.vue";
+export default {
+    components: { GridImg, WorkingProcess, ContactMessage, EmilyFields }
+}
+</script>
